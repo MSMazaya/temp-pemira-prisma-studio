593f7afe861c8de95ca6c48cee1d0f5115bc98ba
MSMazaya <msulthanmazaya@gmail.com>
2022-01-24 00:19:08 +0700

    fix createberkascandidate

--- src/berkas-candidate/dto/create-berkas-candidate.dto.ts
+++ src/berkas-candidate/dto/create-berkas-candidate.dto.ts
@@ -1,9 +1,12 @@
-import { Prisma, ProgressBerkas } from '@prisma/client';
-import { IsNotEmpty } from 'class-validator';
+import { JenisCandidate, Prisma, ProgressBerkas } from "@prisma/client";
+import { IsNotEmpty } from "class-validator";
 
 export class CreateBerkasCandidateDto
   implements Prisma.BerkasCandidateCreateInput
 {
+  id?: string;
+  name: string;
+  jenis_candidate?: JenisCandidate;
   progress: ProgressBerkas;
   BerkasUploaded?: Prisma.BerkasUploadedCreateNestedManyWithoutBerkas_candidateInput;
   @IsNotEmpty()
